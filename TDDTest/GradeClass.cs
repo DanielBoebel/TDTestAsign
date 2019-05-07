b3ce833bf1d488c1cb3ed095ad52836be1de43a5
Daniel Bøbel <daniel.boebel@gmail.com>
2019-05-07 13:47:57 +0200

    TestAsign TDD

---
+++ TDDTest/GradeClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace TDDTest
+{
+    public class GradeClass
+    {
+
+        static void Main()
+		{
+			Console.WriteLine("Hello World!");
+
+		}
+
+    }
+}
---
+++ TDDTest/UnitTest1.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using Xunit;
+
+namespace TDDTest
+{
+    public class UnitTest1
+    {
+		
+
+        [Fact]
+		public void TestGrade()
+        {
+			string testGrade = "C";
+			int tempGrade = -1;
+			ArrayList americanGrade = new ArrayList();
+			americanGrade.Add("A");
+			americanGrade.Add("B");
+			americanGrade.Add("C");
+			americanGrade.Add("D");
+			americanGrade.Add("E");
+			americanGrade.Add("Fx");
+			americanGrade.Add("F");
+
+			if (americanGrade.Contains(testGrade) == true)
+			{
+				tempGrade = americanGrade.IndexOf(testGrade);
+			}
+
+
+			ArrayList danishGrade = new ArrayList();
+			danishGrade.Add("12");
+			danishGrade.Add("10");
+			danishGrade.Add("7");
+			danishGrade.Add("4");
+			danishGrade.Add("02");
+			danishGrade.Add("00");
+			danishGrade.Add("-3");
+
+			Assert.Equal("12", danishGrade[tempGrade]);
+			Assert.Equal("10", danishGrade[tempGrade]);
+			Assert.Equal("7", danishGrade[tempGrade]);
+			Assert.Equal("4", danishGrade[tempGrade]);
+
+
+
+
+        }
+    }
+}
